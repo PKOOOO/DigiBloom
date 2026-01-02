8278cde6203d391aef0dd1999dadfacaf9462f60
pkoooo <gabrielpius0311@gmail.com>
2026-01-02 13:13:19 +0300

    Remove .next folder from Git tracking

--- .next/dev/server/chunks/_next-internal_server_app_api_flowers_[slug]_route_actions_a58ee905.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/api/flowers/[slug]/route/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_api_flowers_%5Bslug%5D_route_actions_a58ee905.js.map
--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"o8KDg9zPQYYEu51Rl7+nDjLax4e9IaYMpY+ZNB4D5jw=\"\n}"
